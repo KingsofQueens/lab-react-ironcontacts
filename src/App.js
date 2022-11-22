c84c659a9d0c0a13dee4ffb5e0ae0e8ae57e4b73
Q <morningqwhy@yahoo.com.hk>
2022-11-22 23:49:48 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactData from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState([
+    contactData[0],
+    contactData[1],
+    contactData[2],
+    contactData[3],
+    contactData[4],
+  ]);
+
+  console.log(contactData);
+
+  const handleAddRandomContact = () => {
+    setContacts([
+      ...contacts,
+      contactData[Math.random(Math.random() * contactData.length) + 6],
+    ]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>IronContacts</h3>
+      <button onClick={handleAddRandomContact}>Add Random Contact</button>
+      <table>
+        <thead>
+          <tr>
+            <td>Picture</td>
+            <td>Name</td>
+            <td>Popularity</td>
+            <td>Won an Oscar</td>
+            <td>Won an Emmy</td>
+          </tr>
+        </thead>
+
+        {contacts.map((value) => {
+          return (
+            <>
+              <tbody key={value}>
+                <tr>
+                  <td key={value.pictureUrl}>
+                    <img src={value.pictureUrl} alt={value.name} />
+                  </td>
+                  <td key={value.name}>{value.name}</td>
+                  <td key={value.popularity}>{value.popularity}</td>
+                  <td>
+                    {(value.wonOscar && <p>🏆</p>) ||
+                      (!value.wonOscar && <p></p>)}
+                  </td>
+                  <td>
+                    {(value.wonEmmy && <p>🏆</p>) ||
+                      (!value.wonEmmy && <p></p>)}
+                  </td>
+                </tr>
+              </tbody>
+            </>
+          );
+        })}
+      </table>
     </div>
   );
 }
-
 export default App;
